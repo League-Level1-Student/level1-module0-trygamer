172135e3d96db255d022481575611f21b1e252a3
trygamer <league@WTS20.attlocal.net>
2018-03-24 11:36:11 -0700

    Fortune_Cookie.ip

--- src/Fortune_Cookie.java
+++ src/Fortune_Cookie.java
@@ -1,4 +1,37 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Fortune_Cookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+public class Fortune_Cookie implements ActionListener {
+
+	public static void main(String[] args) {
+		Fortune_Cookie fc= new Fortune_Cookie();
+		fc.showButton();
+
+
+		
+		
+		
+	}
+	
+ void	showButton(){
+		System.out.println("Button");
+		JFrame f= new JFrame();
+		f.setVisible(true);
+JButton b= new JButton();
+		
+		f.add(b);
+		
+		b.addActionListener(this);
+	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
 }
+}
+
+
